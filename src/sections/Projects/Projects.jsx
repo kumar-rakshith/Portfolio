2819c2f67a46311c0793fab052ce5363b65f9e56
kumar-rakshith <rakshithkumar.thokur@gmail.com>
2024-07-29 20:59:41 +0530

    new

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import styles from './ProjectsStyles.module.css';
-import viberr from '../../assets/viberr.png';
-import freshBurger from '../../assets/fresh-burger.png';
+import Anganawadi_Administration from '../../assets/Anganawadi_Administration.png';
+import Webuild from '../../assets/Webuild.png';
 import hipsster from '../../assets/hipsster.png';
 import fitLift from '../../assets/fitlift.png';
 import ProjectCard from '../../common/ProjectCard';
@@ -11,16 +11,16 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          src={viberr}
+          src={Anganawadi_Administration}
           link="https://github.com/kumar-rakshith/Anganawadi_Administration.git"
           h3="Anganawadi Administration"
-          p="Streaming App"
+          p="Anganawadi Administration is a web application designed to streamline the operations of Anganwadi centers. Built using HTML, CSS, JavaScript for the frontend, and PHP for the backend, with MySQL as the database management system."
         />
         <ProjectCard
-          src={freshBurger}
+          src={Webuild}
           link="https://github.com/kumar-rakshith/WeBuild.git"
-          h3="Fresh Burger"
-          p="Hamburger Restaurant"
+          h3="WeBuild"
+          p="WeBuild is an innovative web-based platform designed to transform the construction industry by enhancing communication, transparency, and project management. It offers a user-friendly interface for clients and robust tools for administrators, ensuring efficient and high-quality construction project execution"
         />
         <ProjectCard
           src={hipsster}
