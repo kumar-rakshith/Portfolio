16fc294e3d05e64914b7572ebd4012b4246ca0c1
kumar-rakshith <rakshithkumar.thokur@gmail.com>
2024-07-30 06:01:25 +0530

    new

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -5,7 +5,7 @@ function ProjectCard({ src, link, h3, p }) {
     <a href={link} target="_blank">
       <img className="hover" src={src} alt={`${h3} logo`} />
       <h3>{h3}</h3>
-      <p>{p}</p>
+      <p className='para'>{p}</p>
     </a>
   );
 }
