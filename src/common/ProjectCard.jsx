7ebf487538fc488fca6013e3ac946d173a4d3e11
kumar-rakshith <rakshithkumar.thokur@gmail.com>
2024-10-08 07:49:27 +0530

    Updating

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 function ProjectCard({ src, link, h3, p }) {
   return (
-    <a href={link} target="_blank">
-      <img className="hover" src={src} alt={`${h3} logo`} />
-      <h3>{h3}</h3>
-      <p className='para'>{p}</p>
+    <a href={link} target="_blank" style={{ textDecoration: 'none', display: 'flex', flexDirection: 'column', gap: '10px', marginTop:'100px' }}>
+      <img className="hover" src={src} alt={`${h3} logo`} style={{ maxWidth: '40%' }} />
+      <h3 style={{ marginTop: '10px' }}>{h3}</h3>
+      <p style={{ marginTop: '5px', marginBottom: '10px', marginLeft:'100px', textAlign:'justify',marginRight:'100px' }}>{p}</p>
     </a>
   );
 }
